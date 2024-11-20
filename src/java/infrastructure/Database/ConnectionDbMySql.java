4c9012556df0eb659939e3f9e86c8cc2634260b3
yeifer alcala <yeiferalcala23@gmail.com>
2024-11-20 01:28:29 -0500

    creamos la coneccion a la base de datos

--- src/java/infrastructure/Database/ConnectionDbMySql.java
+++ src/java/infrastructure/Database/ConnectionDbMySql.java
@@ -4,10 +4,36 @@
  */
 package infrastructure.Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author JEIFER ALCALA
  */
 public class ConnectionDbMySql {
+ 
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/events_web";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            throw new SQLException("Driver not found: " + DRIVER);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new SQLException("Connection error: " + ex.getMessage());
+        }
+
+        return conn;
+    }
+
     
 }
